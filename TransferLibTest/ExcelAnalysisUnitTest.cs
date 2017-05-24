a61b0bcc455c9bed2a80d9c82c289adba9347bde
jaylikecode <2539701829@qq.com>
2017-05-24 11:56:46 +0800

    Excel数据转换为测试用例Model

---
+++ TransferLibTest/ExcelAnalysisUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransferLibrary;
+
+namespace TransferLibTest
+{
+    [TestClass]
+    public class ExcelAnalysisUnitTest
+    {
+        private string _efilePath;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            this._efilePath = @"F:\Code\TestLinkTransfer\trunk\Resource\TestCase.xlsx";
+        }
+
+        [TestMethod]
+        public void OpenExcelTest()
+        {
+            ExcelAnalysis ea = new ExcelAnalysis(this._efilePath);
+            ea.OpenExcel();
+
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
--- TransferLibrary/ExcelAnalysis.cs
+++ TransferLibrary/ExcelAnalysis.cs
@@ -1,9 +1,13 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Microsoft.Office.Interop.Excel;
+using TransferModel;
 
 namespace TransferLibrary
 {
+    //TODO 解析合并格式的Excel数据
     public class ExcelAnalysis
     {
         private readonly string _eFilePath;
@@ -15,7 +19,7 @@ namespace TransferLibrary
 
         public void OpenExcel()
         {
-            object missing = System.Reflection.Missing.Value;
+            object missing = Missing.Value;
             Application excel = new Application();
             if (excel == null)
             {
@@ -30,39 +34,71 @@ namespace TransferLibrary
                 //取得第一个工作薄
                 Worksheet ws = (Worksheet)wb.Worksheets.Item[1];
 
-
-                //取得总记录行数   (包括标题列)
-                int rowsint = ws.UsedRange.Cells.Rows.Count; 
-                //得到行数
-                //int columnsint = mySheet.UsedRange.Cells.Columns.Count;//得到列数
+                var tcList = this.GetExcelData(ws);
 
 
-                //取得数据范围区域 (不包括标题列) 
-                Range rng1 = ws.Cells.get_Range("B2", "B" + rowsint);   //item
 
-
-                Range rng2 = ws.Cells.get_Range("K2", "K" + rowsint); //Customer
-                object[,] arryItem = (object[,])rng1.Value2;   //get range's value
-                object[,] arryCus = (object[,])rng2.Value2;
-                //将新值赋给一个数组
-                string[,] arry = new string[rowsint - 1, 2];
-                for (int i = 1; i <= rowsint - 1; i++)
-                {
-                    //Item_Code列
-                    arry[i - 1, 0] = arryItem[i, 1].ToString();
-                    //Customer_Name列
-                    arry[i - 1, 1] = arryCus[i, 1].ToString();
-                }
+//                //取得总记录行数   (包括标题列)
+//                int rowsint = ws.UsedRange.Cells.Rows.Count; 
+//                //得到行数
+//                int columnsint = ws.UsedRange.Cells.Columns.Count;//得到列数
+//
+//
+//                //取得数据范围区域 (不包括标题列) 
+//                Range rng1 = ws.Cells.get_Range("B2", "J" + rowsint);   //item
+//
+//
+//                Range rng2 = ws.Cells.get_Range("K2", "K" + rowsint); //Customer
+//                object[,] arryItem = (object[,])rng1.Value2;   //get range's value
+//                object[,] arryCus = (object[,])rng2.Value2;
+//                //将新值赋给一个数组
+//                string[,] arry = new string[rowsint - 1, 2];
+//                for (int i = 1; i <= rowsint - 1; i++)
+//                {
+//                    //Item_Code列
+//                    arry[i - 1, 0] = arryItem[i, 1].ToString();
+//                    //Customer_Name列
+//                    arry[i - 1, 1] = arryCus[i, 1].ToString();
+//                }
             }
-            excel.Quit(); excel = null;
-            Process[] procs = Process.GetProcessesByName("Excel");
+            excel.Quit();
+            excel = null;
 
+           
+        }
 
-            foreach (Process pro in procs)
+        public List<TestCase> GetExcelData(Worksheet eWorksheet)
+        {
+            List<TestCase> tcList = new List<TestCase>();
+            int usedRows = eWorksheet.UsedRange.Cells.Rows.Count;
+
+            if (usedRows == 0 || usedRows == 1)
             {
-                pro.Kill();//没有更好的方法,只有杀掉进程
+                throw new Exception("No TestCase!");
             }
-            GC.Collect();
+
+            for (int i = 2; i <= usedRows; i++)
+            {
+                TestCase tc = new TestCase();
+                tc.Name = ((Range) eWorksheet.Cells[i, 1]).Text.ToString();
+                //tc.Importance = (ImportanceType)((Range)eWorksheet.Cells[i, 2]).Text.ToString();
+                tc.ExecutionType = (ExecType) int.Parse(((Range) eWorksheet.Cells[i, 3]).Text.ToString());
+                tc.Keywords = ((Range)eWorksheet.Cells[i, 4]).Text.ToString().Split(',').ToList();
+                tc.Summary = ((Range)eWorksheet.Cells[i, 5]).Text.ToString();
+                tc.Preconditions = ((Range)eWorksheet.Cells[i, 6]).Text.ToString();
+                TestStep ts = new TestStep
+                {
+                    StepNumber = 1,
+                    ExecutionType = ExecType.手动,
+                    Actions = ((Range) eWorksheet.Cells[i, 7]).Text.ToString(),
+                    ExpectedResults = ((Range) eWorksheet.Cells[i, 8]).Text.ToString()
+                };
+                tc.TestSteps = new List<TestStep> {ts};
+
+                tcList.Add(tc);
+            }
+
+            return tcList;
         }
     }
 }
