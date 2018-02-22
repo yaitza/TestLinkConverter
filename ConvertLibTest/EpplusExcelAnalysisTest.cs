84abe43c387dd5da77625643f899b015969d6306
yangzhijie <yangzhijie@aerozhonghuan.com>
2018-02-22 18:40:01 +0800

    添加EpplusExcelAnalysis单元测试

---
+++ ConvertLibTest/EpplusExcelAnalysisTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConvertLibrary;
+
+namespace ConvertLibTest
+{
+    [TestClass]
+    public class EpplusExcelAnalysisTest
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            string filePath = @"E:\Github\TestLinkConverter\Resource\TestCase.xlsx";
+
+            EpplusExcelAnalysis epplusExcelAnalysis = new EpplusExcelAnalysis(filePath);
+        }
+
+        [TestMethod]
+        public void ReadExcelTest()
+        {
+
+        }
+    }
+}
--- ConvertLibrary/EpplusExcelAnalysis.cs
+++ ConvertLibrary/EpplusExcelAnalysis.cs
@@ -2,7 +2,8 @@
 using System.IO;
 using System.Drawing;
 using OfficeOpenXml;
-
+using System.Collections.Generic;
+using TransferModel;
 
 namespace ConvertLibrary
 {
@@ -33,6 +34,23 @@ namespace ConvertLibrary
             {
                 OutputDisplay.ShowMessage(ex.Message, Color.Red);
                 return;
+            }
+        }
+
+        public void ReadExcel()
+        {
+            int iCount = this.excelPackage.Workbook.Worksheets.Count;
+
+            if(iCount == 0)
+            {
+                OutputDisplay.ShowMessage("表中无Sheet页！", Color.Red);
+                return;
+            }
+
+            for(int iFlag =0; iFlag<iCount; iCount++)
+            {
+                ExcelWorksheet excelWorksheet = this.excelPackage.Workbook.Worksheets[iFlag];
+
             }
         }
 
