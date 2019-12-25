2fb0614b3a8d0b18d43c7c9eac1c6a0df1c17c67
yangzhijie <yangzhijie@aerozhonghuan.com>
2019-12-25 16:21:55 +0800

    解决测试套嵌套不同层级存在同名的情况

--- ConvertModel/TestSuite.cs
+++ ConvertModel/TestSuite.cs
@@ -10,6 +10,19 @@ namespace ConvertModel
 
         public List<TestCase> TestCases { get; set; }
 
+        public string UnqiueCode
+        {
+            get
+            {
+                string str = string.Empty;
+                foreach (string s in NameHierarchy)
+                {
+                    str += s + ",";
+                }
+                return str.TrimEnd(',');
+            }
+        }
+
         public string TestLinkStr { get; set; }
     }
 }
